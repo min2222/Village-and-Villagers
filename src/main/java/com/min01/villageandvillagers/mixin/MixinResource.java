1ca41d537fb52b24ee011c370551a487e932d2c2
min01 <goyoumin2@gmail.com>
2025-11-23 00:15:46 +0900

    Update MixinResource.java

--- src/main/java/com/min01/villageandvillagers/mixin/MixinResource.java
+++ src/main/java/com/min01/villageandvillagers/mixin/MixinResource.java
@@ -5,42 +5,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.min01.villageandvillagers.AESUtil;
 
-import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.resources.IoSupplier;
 import net.minecraft.server.packs.resources.Resource;
-import net.minecraft.server.packs.resources.ResourceMetadata;
 
-@Mixin(Resource.class)
+@Mixin(value = Resource.class, priority = -15000)
 public class MixinResource 
 {
-    @Unique
-    private IoSupplier<InputStream> stream;
-
-    @Inject(method = "<init>(Lnet/minecraft/server/packs/PackResources;Lnet/minecraft/server/packs/resources/IoSupplier;Lnet/minecraft/server/packs/resources/IoSupplier;)V", at = @At("TAIL"), cancellable = true)
-    private void init(PackResources p_250802_, IoSupplier<InputStream> p_248585_, IoSupplier<ResourceMetadata> p_250094_, CallbackInfo ci) 
-    {
-        this.stream = p_248585_;
-    }
-
-    @Inject(method = "<init>(Lnet/minecraft/server/packs/PackResources;Lnet/minecraft/server/packs/resources/IoSupplier;)V", at = @At("TAIL"), cancellable = true)
-    private void init2(PackResources p_250372_, IoSupplier<InputStream> p_248749_, CallbackInfo ci)
-    {
-        this.stream = p_248749_;
-    }
-
+	@Shadow
+	@Final
+	private IoSupplier<InputStream> streamSupplier;
+	   
     @Inject(method = "open", at = @At("HEAD"), cancellable = true)
     private void open(CallbackInfoReturnable<InputStream> cir) throws IOException 
     {
-        byte[] data = this.stream.get().readAllBytes();
+        byte[] data = this.streamSupplier.get().readAllBytes();
         if(data.length > 8 && new String(data, 0, 8, StandardCharsets.UTF_8).equals(AESUtil.HEADER)) 
         {
 			try 
@@ -52,6 +39,10 @@ public class MixinResource
 			{
 				e.printStackTrace();
 			}
+        }
+        else
+        {
+            cir.setReturnValue(new ByteArrayInputStream(data));
         }
     }
 }
